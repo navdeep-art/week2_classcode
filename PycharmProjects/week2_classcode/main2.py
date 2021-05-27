ad696a471ea9464d4ef37ccd3591bed6ae9ebbff
Admin <navdeep-art>
2021-05-27 01:56:13 -0400

    Committing modules

---
+++ PycharmProjects/week2_classcode/main2.py
@@ -0,0 +1,16 @@
+# Add module here using import
+import main
+import reverse
+
+from main import areaofcircle
+from reverse import name
+
+
+if __name__ == '__main__':
+
+    print(main.areaofcircle())
+    print(reverse.name())
+
+
+
+
---
+++ PycharmProjects/week2_classcode/reverse.py
@@ -0,0 +1,4 @@
+def name():
+    firstname = str(" Navdeep")
+    lastname = str (" Kaur")
+    print (" Reverse order of my name is:"+lastname+" "+firstname)
