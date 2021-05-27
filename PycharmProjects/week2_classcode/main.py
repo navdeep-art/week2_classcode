765cc4110550509211e62248d1cf5de2ad8659c7
Admin <navdeep-art>
2021-05-27 01:53:23 -0400

    Initial COmmit

---
+++ PycharmProjects/week2_classcode/main.py
@@ -0,0 +1,5 @@
+def areaofcircle():
+    r = float(input("Enter the radius of the circle:"))
+    pi = 3.14
+    areaofcircle = (pi * r * 2)
+    print("The area of the circle is: " + str(areaofcircle))
